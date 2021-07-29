c5dee7f6216b0fc3292c89020be98cc747d84cbc
Victor Katsuo Yamashiro <vkatsuo@gmail.com>
2021-07-29 01:06:33 -0300

    add controlled input in general data

--- src/components/generalData/GeneralData.jsx
+++ src/components/generalData/GeneralData.jsx
@@ -1,21 +1,27 @@
 /* eslint-disable jsx-a11y/no-access-key */
-import React from 'react';
+import React, { useState } from 'react';
 import * as Styled from './GeneralData.style';
 
-const GeneralData = () => (
-  <>
-    <Styled.GeneralDataContainer>
-      <Styled.Label htmlFor="form_name" accessKey="1">
-        Nome:
-        <Styled.Input type="text" id="form_name" name="form_name" placeholder="Escreva o seu nome" />
-      </Styled.Label>
+const GeneralData = () => {
+  const [state, setState] = useState('');
 
-      <Styled.Label htmlFor="form_surname">
-        Sobrenome:
-        <Styled.Input type="text" id="form_surname" surname="form_surname" placeholder="Escreva o seu sobrenome" />
-      </Styled.Label>
-    </Styled.GeneralDataContainer>
-  </>
-);
+  const handleChange = (event) => { setState(event.target.value); };
+
+  return (
+    <>
+      <Styled.GeneralDataContainer>
+        <Styled.Label htmlFor="form_name" accessKey="1">
+          Nome:
+          <Styled.Input type="text" value={state} onChange={handleChange} id="form_name" name="form_name" placeholder="Escreva o seu nome" />
+        </Styled.Label>
+
+        <Styled.Label htmlFor="form_surname">
+          Sobrenome:
+          <Styled.Input type="text" value={state} onChange={handleChange} id="form_surname" surname="form_surname" placeholder="Escreva o seu sobrenome" />
+        </Styled.Label>
+      </Styled.GeneralDataContainer>
+    </>
+  );
+};
 
 export default GeneralData;
