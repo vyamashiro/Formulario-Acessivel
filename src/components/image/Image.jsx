b291b0e7b20c08082d8d6f70ffdb2cc5e3d6d69e
vyamashiro <vkatsuo@gmail.com>
2022-02-05 15:10:08 -0300

    ajust image

--- src/components/image/Image.jsx
+++ src/components/image/Image.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-trailing-spaces */
 import React from 'react';
 import Picture from './man-with-laptop-thinking.jpg';
 import * as Styled from './Image.style';
@@ -15,7 +16,7 @@ function Image() {
             alt="foto de um homem pensativo sentado no chão com as pernas cruzadas e um notebook em seu colo."
           />
           <Styled.Figcaption>
-            créditos:
+            créditos: 
             <Styled.Link
               target="_blank"
               rel="noreferrer noopener"
@@ -26,17 +27,22 @@ function Image() {
           </Styled.Figcaption>
         </figure>
         <Styled.TitleH2>Atributo alt</Styled.TitleH2>
-        <Styled.Paragraph>Imagens precisam ter um atributo alt dentro da tag img para que possam ser lidas por leitores de tela.</Styled.Paragraph>
+        <Styled.Paragraph>
+          Imagens precisam ter um atributo alt dentro da tag img para que possam ser lidas por leitores de tela.
+        </Styled.Paragraph>
         <Styled.TitleH2>Como escrever o atributo alt</Styled.TitleH2>
-        <Styled.Paragraph>Para descrição do atributo alt é importante seguir algumas boas práticas que são:</Styled.Paragraph>
+        <Styled.Paragraph>
+          Para descrição do atributo alt é importante seguir algumas boas práticas que são:
+        </Styled.Paragraph>
         <Styled.Ul>
-          <Styled.Li>UtiStyled.Lize frases curtas e evite adjetivos;</Styled.Li>
+          <Styled.Li>Utilize frases curtas e evite adjetivos;</Styled.Li>
           <Styled.Li>Classifique o tipo da imagem (fotografia, tirinha, ilustração e etc);</Styled.Li>
-          <Styled.Li>Descreva a imagem seguindo esta ordem: da esquerda para a direita e, posteriormente, de cima para baixo;</Styled.Li>
-          <Styled.Li>Informe cores, tonaStyled.Lidades;</Styled.Li>
+          <Styled.Li>
+            Descreva a imagem seguindo esta ordem: da esquerda para a direita e, posteriormente, de cima para baixo;
+          </Styled.Li>
+          <Styled.Li>Informe cores, tonalidades;</Styled.Li>
           <Styled.Li>Descreva os elementos em uma sequência lógica.</Styled.Li>
         </Styled.Ul>
-        <Styled.Paragraph>Baseado no artigo do site da site...</Styled.Paragraph>
       </article>
     </main>
   );
